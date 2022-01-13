46400be5c99435383324c370aa1f4d41e5e53572
Shane <MalithSM@hotmail.com>
2022-01-12 23:44:17 -0600

    commit tree dfff3aca5abc4839c3366bea1c072a21c6b10fde

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(names, argument) {
+  return names.filter((person) => {
+    return person.toLowerCase() === argument.toLowerCase();
+  });
+}
+
+// function fuzzyMatch(drivers, string) {
+//   return drivers.filter((person) => {
+//     return person. === string.substring();
+//   });
+// }
+
+function matchName(objects, string) {
+  return objects.filter((newElement) => {
+    return newElement.name === string;
+  });
+}
+
+const example = {
+  hometown: "Tampa Bay",
+  name: "Bobby",
+};
